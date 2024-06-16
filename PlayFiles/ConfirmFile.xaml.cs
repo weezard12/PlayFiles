9449401f4d832325733f93fd027e70e54ce3a23f
weezard12 <weezard1234@gmail.com>
2024-06-16 13:14:49 +0300

    Add project files.

---
+++ PlayFiles/ConfirmFile.xaml.cs
@@ -0,0 +1,39 @@
+﻿using PlayFiles.Logic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PlayFiles
+{
+    /// <summary>
+    /// Interaction logic for ConfirmFile.xaml
+    /// </summary>
+    public partial class ConfirmFile : Window
+    {
+        Logic.FileInfo FileInfo;
+        public ConfirmFile(Logic.FileInfo fileInfo)
+        {
+            InitializeComponent();
+        }
+        public void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        public void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            Logic.FileInfo.filesLoaded.Add(FileInfo);
+            Close();
+        }
+    }
+}
---
+++ PlayFiles/Logic/FileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayFiles.Logic
+{
+    public class FileInfo
+    {
+        public static List<FileInfo> filesLoaded = new List<FileInfo>();
+
+        string path;
+        string name;
+        DateTime activeTime;
+        public FileInfo(string path)
+        {
+            this.path = path;
+            activeTime = DateTime.Now.AddHours(1);
+        }
+    }
+}
